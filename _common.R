02ed1a81fdc218ac96d83b997f9897b54d765f79
Ariana <arianacabral57@gmail.com>
2025-02-10 10:27:26 -0300

    initial version of the book

---
+++ _common.R
@@ -0,0 +1,20 @@
+knitr::opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE,
+  fig.retina = 2,
+  fig.width = 6,
+  fig.asp = 2/3,
+  fig.show = "hold"
+)
+
+options(
+  dplyr.print_min = 6,
+  dplyr.print_max = 6,
+  pillar.max_footer_lines = 2,
+  pillar.min_chars = 15,
+  stringr.view_n = 6,
+  cli.num_colors = 0,
+  cli.hyperlink = FALSE,
+  pillar.bold = TRUE,
+  width = 77
+)
